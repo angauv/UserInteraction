a1433d34d5c607668038d5b5c614a19fe35322fd
Andy <aangauv@gmail.com>
2017-01-30 22:36:12 -0800

    Assignment 1(c): Swipe works and double tap to return to activity A works. Return sets image that Activity B currently looks at.

--- app/src/main/java/com/example/server/userinteraction/Main2Activity.java
+++ app/src/main/java/com/example/server/userinteraction/Main2Activity.java
@@ -1,20 +1,130 @@
+/*
+        Modified by: Andy Ngauv
+        ID: A00839270
+ */
+
 package com.example.server.userinteraction;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.widget.ImageView;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.MotionEvent;
+import android.widget.Toast;
+import android.widget.TextView;
+import android.view.GestureDetector.OnGestureListener;
 
 
-public class Main2Activity extends AppCompatActivity {
+//public class Main2Activity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        Intent intent = getIntent();
-        int imageID = intent.getIntExtra("image_id", 0);
-        ImageView image = (ImageView)findViewById(R.id.imageView1);
-        image.setImageResource(imageID);
+    public class Main2Activity extends AppCompatActivity implements OnClickListener {
+        //public class Main2Activity extends AppCompatActivity implements OnGestureListener{
+        private static final int SWIPE_MIN_DISTANCE = 10;
+        private static final int SWIPE_MAX_OFF_PATH = 250;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+        private GestureDetector gestureDetector;
+        View.OnTouchListener gestureListener;
+        private TextView myTextView;
+        private static int clickCount = 1;
+        private int imageGet = 0;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main2);
+            Intent intent = getIntent();
+            Bundle extras = intent.getExtras();
+            int nextImage = extras.getInt("image_id", 0);
+            int imageSelect[] = extras.getIntArray("imageArray");
+           // int imageID = intent.getIntExtra("image_id",0);
+            ImageView image = (ImageView) findViewById(R.id.imageView1);
+            image.setImageResource(imageSelect[nextImage]);
+
+            myTextView = new TextView(this);
+            myTextView.setTextSize(16);
+            image.setOnClickListener(this);
+            gestureDetector = new GestureDetector(this, new MyGestureDetector());
+            gestureListener = new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    return gestureDetector.onTouchEvent(event);
+                }
+            };
+            image.setOnTouchListener(gestureListener);
+            //gestureScanner = new GestureDetector(getBaseContext(), this);
+        }
+        class MyGestureDetector extends SimpleOnGestureListener {
+            Intent intent = getIntent();
+            Bundle extras = intent.getExtras();
+            int nextImage = extras.getInt("image_id", 0);
+            int imageSelect[] = extras.getIntArray("imageArray");
+            int imageStart = extras.getInt("imageStart",0);
+            int imageEnd = extras.getInt("imageEnd",0);
+            ImageView image = (ImageView) findViewById(R.id.imageView1);
+            @Override
+            public boolean onFling(MotionEvent me1, MotionEvent me2, float vX, float vY) {
+                try {
+                        /*use me1.getX(), me1.getY(), me2.getX() and me2.getY()
+                        to determine the length of the swipe action along x and/or y directions.
+                        The parameters vx and vy  are the velocities of the swipe action along  x and y directions, respectively.
+                        Use these to detect left and right swipes.  Upon detecting that left or right swipe indeed took place,
+                        just rotate the images in the image view in the corresponding direction, as in Activity 1*/
+
+                    if(Math.abs(me1.getX()-me2.getX()) > SWIPE_MAX_OFF_PATH)
+                        return false;
+
+                    // swipe left
+                    if (me1.getX()-me2.getX() > SWIPE_MIN_DISTANCE){
+                        Toast.makeText(Main2Activity.this, "left", Toast.LENGTH_SHORT).show();
+
+                        if (nextImage == imageStart)
+                            nextImage = imageEnd;
+                        else
+                            nextImage = nextImage -1;
+                        imageGet = imageSelect[nextImage];
+                        image.setImageResource(imageGet);
+                }
+                    // swipe right
+                    else if(me2.getX()-me1.getX() > SWIPE_MIN_DISTANCE){
+                        Toast.makeText(Main2Activity.this, "right", Toast.LENGTH_SHORT).show();
+
+                        if (nextImage == imageEnd)
+                            nextImage = imageStart;
+                        else
+                            nextImage = nextImage + 1;
+                        imageGet = imageSelect[nextImage];
+                        image.setImageResource(imageGet);
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("Exception: No swipe detected");
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onDoubleTap(MotionEvent e){
+                Intent intent = new Intent();
+                intent.putExtra("image_id", imageGet);
+                setResult(1,intent);
+                finish();
+                return true;
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+          /*  if (clickCount ==2) {
+                Intent intent = new Intent();
+                intent.putExtra("image_id", 0);
+                finish();
+                clickCount = 1;
+            }
+            else
+                clickCount++;*/
+        }
     }
-}
--- app/src/main/java/com/example/server/userinteraction/MainActivity.java
+++ app/src/main/java/com/example/server/userinteraction/MainActivity.java
@@ -1,50 +1,105 @@
+/*
+        Modified by: Andy Ngauv
+        ID: A00839270
+ */
+
 package com.example.server.userinteraction;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.*;
+
+import static android.R.attr.data;
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     ImageView image = null;
     int imageID;
+    int imageSelect [] = {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d}; // Array of images
+    int nextImage = 0; // go to next or previous image, default image is .a picture
+    int i = 0;
 
+/*
+    public void imageFile(String fname) {
+        File folder = new File(fname);
+        File[] listOfFiles = folder.listFiles();
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                imageSel[i] = Integer.parseInt(file.getName());
+                i++;
+            }
+        }
+    }
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         image = (ImageView)findViewById(R.id.imageView1);
 
- /*       Button aButton = (Button) findViewById(R.id.button_a);
-        aButton.setOnClickListener(this);
+        // Display default image at the beginning
+        image.setImageResource(imageSelect[nextImage]);
+        imageID = imageSelect[nextImage];
 
-        Button bButton = (Button) findViewById(R.id.button_b);
-        bButton.setOnClickListener(this);
-*/
-        Button cButton = (Button) findViewById(R.id.button_left);
+        Button leftButton = (Button) findViewById(R.id.button_left);
         leftButton.setOnClickListener(leftButtonListener);
-
     }
     private OnClickListener leftButtonListener = new OnClickListener() {
         public void onClick(View v) {
-            image.setImageResource(R.drawable.c);
-            imageID = R.drawable.c;
+            // Determines if image is at beginning of gallery, if not scroll to next image
+            if(nextImage == 0)
+                nextImage = 3;
+            else
+                nextImage--;
+
+            image.setImageResource(imageSelect[nextImage]);
+            imageID = imageSelect[nextImage];
         }
     };
     public void rightClick(View v) {
-        image.setImageResource(R.drawable.d);
-        imageID = R.drawable.d;
+        // Determine is at the end of gallery, if not scroll to next image
+        if(nextImage == 3)
+            nextImage = 0;
+        else
+            nextImage++;
+
+        image.setImageResource(imageSelect[nextImage]);
+        imageID = imageSelect[nextImage];
     }
     public void goClick(View v) {
         Intent intent = new Intent(this, Main2Activity.class);
-        intent.putExtra("image_id", imageID);
-        startActivity(intent);
-
+        Bundle extras = new Bundle();
+        extras.putIntArray("imageArray",imageSelect);
+        extras.putInt("image_id",nextImage);
+        extras.putInt("imageStart",0);
+        extras.putInt("imageEnd",3);
+        intent.putExtras(extras);
+        //intent.putExtra("image_id", imageID);
+        startActivityForResult(intent,1);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1){
+            imageID = data.getIntExtra("image_id",0);
+            image.setImageResource(imageID);
+        }
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -52,7 +107,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 image.setImageResource(R.drawable.a);
                 imageID = R.drawable.a;
                 break;
-            case R.id.button_b:
+            case R.id.button_right:
                 image.setImageResource(R.drawable.b);
                 break;
         }
