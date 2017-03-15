2e1d18235a9db8388cceb5be3fa3725c6a06612b
angauv <778a94e7>
2017-03-15 12:17:18 -0700

    update

--- app/src/main/java/com/example/server/userinteraction/Main2Activity.java
+++ app/src/main/java/com/example/server/userinteraction/Main2Activity.java
@@ -5,10 +5,19 @@
 
 package com.example.server.userinteraction;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.hardware.SensorEventListener;
+import android.icu.text.StringSearch;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.ImageView;
 import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
@@ -19,105 +28,280 @@ import android.widget.Toast;
 import android.widget.TextView;
 import android.view.GestureDetector.OnGestureListener;
 
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import static java.lang.Math.*;
 
 //public class Main2Activity extends AppCompatActivity {
 
-    public class Main2Activity extends AppCompatActivity implements OnClickListener {
-        //public class Main2Activity extends AppCompatActivity implements OnGestureListener{
-        private static final int SWIPE_MIN_DISTANCE = 10;
-        private static final int SWIPE_MAX_OFF_PATH = 250;
-        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
-        private GestureDetector gestureDetector;
-        View.OnTouchListener gestureListener;
-        private TextView myTextView;
+public class Main2Activity extends AppCompatActivity implements OnClickListener {
+    //public class Main2Activity extends AppCompatActivity implements OnGestureListener{
+    private static final int SWIPE_MIN_DISTANCE = 100;
+    private static final int SWIPE_MAX_OFF_PATH = 250;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+    private static final float SHAKE_MIN = (float)18.0;
+    private static final float SNAP_MIN = (float)16.0;
+    private GestureDetector gestureDetector;
+    View.OnTouchListener gestureListener;
+    private TextView myTextView;
+
+    private TextView textXaccel;
+    private TextView textYaccel;
+    private TextView textZaccel;
+
+    private TextView textXgyro;
+    private TextView textYgyro;
+    private TextView textZgyro;
+
+    private SensorManager mSensorManager;
+    private Sensor acceleration;
+    private Sensor gyro;
+
+    private String getPath;
+    private float xLast;
+    private boolean accelInit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        ImageView image = (ImageView) findViewById(R.id.imageView1);
+
+        String imgPath = intent.getStringExtra("imagePath");
+        File file = new File(imgPath);
+        Uri imgUri = Uri.fromFile(file);
+        image.setImageURI(imgUri);
+        getPath = imgPath;
+
+        myTextView = new TextView(this);
+        myTextView.setTextSize(16);
+        image.setOnClickListener(this);
+        gestureDetector = new GestureDetector(this, new MyGestureDetector());
+        gestureListener = new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }
+        };
+        image.setOnTouchListener(gestureListener);
+
+        textXaccel = (TextView) findViewById(R.id.TextViewXaccel);
+        textYaccel = (TextView) findViewById(R.id.TextViewYaccel);
+        textZaccel = (TextView) findViewById(R.id.TextViewZaccel);
+
+        textXgyro = (TextView) findViewById(R.id.TextViewXgyro);
+        textYgyro = (TextView) findViewById(R.id.TextViewYgyro);
+        textZgyro = (TextView) findViewById(R.id.TextViewZgyro);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        gyro = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        acceleration = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(mSensorListener, gyro, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(mSensorListener, acceleration, SensorManager.SENSOR_DELAY_NORMAL);
+        accelInit = false;
+
+        //gestureScanner = new GestureDetector(getBaseContext(), this);
+    }
+
+    class MyGestureDetector extends SimpleOnGestureListener {
+        Intent intent = getIntent();
+        ImageView image = (ImageView) findViewById(R.id.imageView1);
+
+        File file = new File(getPath);
+        Uri imgUri = Uri.fromFile(file);
+        File[] files = new File(file.getParent()).listFiles();
+        String[] fileString = new File(file.getParent()).list();
+        int nextImage = Arrays.binarySearch(fileString, file.getName());
 
         @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main2);
-            Intent intent = getIntent();
-            Bundle extras = intent.getExtras();
-            int nextImage = extras.getInt("nextImage", 0);
-            int imageSelect[] = extras.getIntArray("imageArray");
-           // int imageID = intent.getIntExtra("image_id",0);
-            ImageView image = (ImageView) findViewById(R.id.imageView1);
-            image.setImageResource(imageSelect[nextImage]);
-
-            myTextView = new TextView(this);
-            myTextView.setTextSize(16);
-            image.setOnClickListener(this);
-            gestureDetector = new GestureDetector(this, new MyGestureDetector());
-            gestureListener = new View.OnTouchListener() {
-                public boolean onTouch(View v, MotionEvent event) {
-                    return gestureDetector.onTouchEvent(event);
-                }
-            };
-            image.setOnTouchListener(gestureListener);
-            //gestureScanner = new GestureDetector(getBaseContext(), this);
-        }
-        class MyGestureDetector extends SimpleOnGestureListener {
-            Intent intent = getIntent();
-            Bundle extras = intent.getExtras();
-            int nextImage = extras.getInt("nextImage", 0);
-            int imageSelect[] = extras.getIntArray("imageArray");
-            int imageStart = extras.getInt("imageStart",0);
-            int imageEnd = extras.getInt("imageEnd",0);
-            ImageView image = (ImageView) findViewById(R.id.imageView1);
-
-            @Override
-            public boolean onFling(MotionEvent me1, MotionEvent me2, float vX, float vY) {
-                try {
+        public boolean onFling(MotionEvent me1, MotionEvent me2, float vX, float vY) {
+            try {
                         /*use me1.getX(), me1.getY(), me2.getX() and me2.getY()
                         to determine the length of the swipe action along x and/or y directions.
                         The parameters vx and vy  are the velocities of the swipe action along  x and y directions, respectively.
                         Use these to detect left and right swipes.  Upon detecting that left or right swipe indeed took place,
                         just rotate the images in the image view in the corresponding direction, as in Activity 1*/
 
-                    if(Math.abs(me1.getX()-me2.getX()) > SWIPE_MAX_OFF_PATH)
-                        return false;
+                if (Math.abs(me1.getX() - me2.getX()) > SWIPE_MAX_OFF_PATH)
+                    return false;
 
-                    // swipe left and set next image in array/gallery
-                    if (me1.getX()-me2.getX() > SWIPE_MIN_DISTANCE){
-                        Toast.makeText(Main2Activity.this, "left", Toast.LENGTH_SHORT).show();
-
-                        if (nextImage == imageStart)
-                            nextImage = imageEnd;
-                        else
-                            nextImage--;
-
-                        image.setImageResource(imageSelect[nextImage]);
+                // swipe left
+                if (me1.getX() - me2.getX() > SWIPE_MIN_DISTANCE) {
+                    onSwipeLeft();
                 }
-                    // swipe right and set next image in array/gallery
-                    else if(me2.getX()-me1.getX() > SWIPE_MIN_DISTANCE){
-                        Toast.makeText(Main2Activity.this, "right", Toast.LENGTH_SHORT).show();
-
-                        if (nextImage == imageEnd)
-                            nextImage = imageStart;
-                        else
-                            nextImage++;
-
-                        image.setImageResource(imageSelect[nextImage]);
-                    }
-
-                } catch (Exception e) {
-                    System.out.println("Exception: No swipe detected");
+                // swipe right
+                else if (me2.getX() - me1.getX() > SWIPE_MIN_DISTANCE) {
+                    onSwipeRight();
                 }
-                return false;
+                // swipe Up
+                else if (me1.getY() - me2.getY() > SWIPE_MIN_DISTANCE) {
+                    onSwipeUp();
+                }
+                // swipe Down
+                else if (me2.getY() - me1.getY() > SWIPE_MIN_DISTANCE) {
+                    onSwipeDown();
+                }
+
+            } catch (Exception e) {
+                Log.d("Swipe","Exception: No swipe detected");
             }
+            return false;
+        }
 
-            // Return to Activity A when user double taps screen and set current image in Activity B
-            // to Activity A imageview.
-            @Override
-            public boolean onDoubleTap(MotionEvent e){
-                Intent intent = new Intent();
-                intent.putExtra("imageID", imageSelect[nextImage]);
-                setResult(1,intent);
-                finish();
-                return true;
+        // Return to Activity A when user double taps screen and set current image in Activity B
+        // to Activity A imageview.
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Intent intent = new Intent();
+            //intent.putExtra("imagePath", files[nextImage].getAbsolutePath());
+            intent.putExtra("imagePath", getPath);
+            setResult(1, intent);
+            finish();
+            return true;
+        }
+
+        public void onSwipeUp(){
+            Toast.makeText(Main2Activity.this, "up", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent();
+            intent.putExtra("imagePath", files[nextImage].getAbsolutePath());
+            setResult(1, intent);
+            finish();
+        }
+        public void onSwipeDown(){
+            Toast.makeText(Main2Activity.this, "down", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent();
+            intent.putExtra("imagePath", files[nextImage].getAbsolutePath());
+            setResult(1, intent);
+            finish();
+        }
+
+        public void onSwipeLeft() {
+            Toast.makeText(Main2Activity.this, "left", Toast.LENGTH_SHORT).show();
+
+            if (nextImage == 0)
+                nextImage = files.length - 1;
+            else
+                nextImage--;
+
+            imgUri = Uri.fromFile(files[nextImage]);
+            image.setImageURI(imgUri);
+            getPath = files[nextImage].getAbsolutePath();
+        }
+
+        public void onSwipeRight() {
+            Toast.makeText(Main2Activity.this, "right", Toast.LENGTH_SHORT).show();
+
+            if (nextImage == (files.length - 1))
+                nextImage = 0;
+            else
+                nextImage++;
+
+            imgUri = Uri.fromFile(files[nextImage]);
+            image.setImageURI(imgUri);
+            getPath = files[nextImage].getAbsolutePath();
+        }
+    }
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE){
+                getGyro(event);
+            }
+            else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+                getAccelerometer(event);
             }
         }
 
         @Override
-        public void onClick(View v) {}
+        public void onAccuracyChanged(Sensor sensor, int accuracy){}
 
-    }
+        private void getGyro(SensorEvent event){
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            textXgyro.setText((int)x + " rad/s");
+            textYgyro.setText((int)y + " rad/s");
+            textZgyro.setText((int)z + " rad/s");
+
+            if (Math.abs(x)+Math.abs(y)+Math.abs(z) > SHAKE_MIN){
+                Intent intent = new Intent();
+                intent.putExtra("imagePath", getPath);
+                setResult(2, intent);
+                finish();
+            }
+        }
+        private void getAccelerometer(SensorEvent event){
+            ImageView image = (ImageView) findViewById(R.id.imageView1);
+            File file = new File(getPath);
+            Uri imgUri;
+            File[] files = new File(file.getParent()).listFiles();
+            String[] fileString = new File(file.getParent()).list();
+            int nextImage = Arrays.binarySearch(fileString, file.getName());
+
+            // Display default image at the beginning
+            for (int i = 0; i < files.length ; i++){
+                File testFile = files[i];
+            }
+
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            DecimalFormat dF = new DecimalFormat("#.##");
+            x = Float.parseFloat(dF.format(x));
+            y = Float.parseFloat(dF.format(y));
+            z = Float.parseFloat(dF.format(z));
+
+            textXaccel.setText(String.valueOf(x) + " m/s2");
+            textYaccel.setText(String.valueOf(y) + " m/s2");
+            textZaccel.setText(String.valueOf(z) + " m/s2");
+
+            if(!accelInit) {
+                xLast = x;
+                accelInit = true;
+            }
+            else {
+                float deltaX = xLast - x;
+                xLast = x;
+
+                if (deltaX < -SNAP_MIN) {
+                    if (nextImage == 0)
+                        nextImage = files.length -1;
+                    else
+                        nextImage--;
+
+                    imgUri = Uri.fromFile(files[nextImage]);
+                    image.setImageURI(imgUri);
+                    getPath = files[nextImage].getAbsolutePath();
+                }
+                else if (deltaX > SNAP_MIN) {
+                    if (nextImage == files.length -1)
+                        nextImage = 0;
+                    else
+                        nextImage++;
+
+                    imgUri = Uri.fromFile(files[nextImage]);
+                    image.setImageURI(imgUri);
+                    getPath = files[nextImage].getAbsolutePath();
+                }
+            }
+        }
+
+        private void onResume(){
+            Main2Activity.super.onResume();
+            mSensorManager.registerListener(this,acceleration,SensorManager.SENSOR_DELAY_NORMAL);
+            mSensorManager.registerListener(this,gyro,SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        protected void onPause(){
+            Main2Activity.super.onPause();
+            mSensorManager.unregisterListener(this);
+        }
+    };
+
+    @Override
+    public void onClick(View v) {}
+
+}
--- app/src/main/java/com/example/server/userinteraction/MainActivity.java
+++ app/src/main/java/com/example/server/userinteraction/MainActivity.java
@@ -10,6 +10,10 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,31 +21,23 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.*;
-
-import static android.R.attr.data;
-import static java.security.AccessController.getContext;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     ImageView image = null;
-    int imageID;
-    int imageSelect [] = {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d}; // Array of images
     int nextImage = 0; // go to next or previous image, default image is .a picture
     int i = 0;
+    String path = Environment.getExternalStorageDirectory().toString() + "/Pictures/";
+    //private String path = "/mnt/sdcard/Pictures/";
+    //private File[] files = new File(path).listFiles();
+    static final int REQUEST_TAKE_PHOTO =1;
+    String photoPath;
 
-/*
-    public void imageFile(String fname) {
-        File folder = new File(fname);
-        File[] listOfFiles = folder.listFiles();
-        for (File file : listOfFiles) {
-            if (file.isFile()) {
-                imageSel[i] = Integer.parseInt(file.getName());
-                i++;
-            }
-        }
-    }
-*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,44 +45,50 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
         image = (ImageView)findViewById(R.id.imageView1);
 
-        // Display default image at the beginning
-        image.setImageResource(imageSelect[nextImage]);
-        imageID = imageSelect[nextImage];
+        File[] files = new File(path).listFiles();
+        String imgPath = path + files[0].getName();
+        File file = new File(imgPath);
+        Uri imgUri = Uri.fromFile(file);
+        image.setImageURI(imgUri);
 
         Button leftButton = (Button) findViewById(R.id.button_left);
         leftButton.setOnClickListener(leftButtonListener);
     }
     private OnClickListener leftButtonListener = new OnClickListener() {
         public void onClick(View v) {
+            File[] files = new File(path).listFiles();
             // Determines if image is at beginning of gallery, if not scroll to next image
             if(nextImage == 0)
-                nextImage = 3;
+                nextImage = files.length-1;
             else
                 nextImage--;
 
-            image.setImageResource(imageSelect[nextImage]);
-            imageID = imageSelect[nextImage];
+            String imgPath = path + files[nextImage].getName();
+            File file = new File(imgPath);
+            Uri imgUri = Uri.fromFile(file);
+            image.setImageURI(imgUri);
+            Toast.makeText(MainActivity.this, imgPath, Toast.LENGTH_SHORT).show();
         }
     };
     public void rightClick(View v) {
+        File[] files = new File(path).listFiles();
         // Determine is at the end of gallery, if not scroll to next image
-        if(nextImage == 3)
+        if(nextImage == files.length-1)
             nextImage = 0;
         else
             nextImage++;
 
-        image.setImageResource(imageSelect[nextImage]);
-        imageID = imageSelect[nextImage];
+        String imgPath = path + files[nextImage].getName();
+        File file = new File(imgPath);
+        Uri imgUri = Uri.fromFile(file);
+        image.setImageURI(imgUri);
+        Toast.makeText(MainActivity.this, imgPath, Toast.LENGTH_SHORT).show();
     }
     public void goClick(View v) {
+        File[] files = new File(path).listFiles();
         Intent intent = new Intent(this, Main2Activity.class);
-        Bundle extras = new Bundle();
-        extras.putIntArray("imageArray",imageSelect);
-        extras.putInt("nextImage",nextImage);
-        extras.putInt("imageStart",0);
-        extras.putInt("imageEnd",3);
-        intent.putExtras(extras);
-        startActivityForResult(intent,1);
+        intent.putExtra("imagePath", files[nextImage].getAbsolutePath());
+        startActivityForResult(intent,2);
     }
 
     // Resume the scrolling of images where activity B left off in Activty A
@@ -94,14 +96,58 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == 1){
-            imageID = data.getIntExtra("imageID",0);
-            image.setImageResource(imageID);
+        //if(requestCode == REQUEST_TAKE_PHOTO){
+        //  String filePath = data.getDataString();
+        // File testFile = new File(filePath);
+        //int i = 1;
+        //}
+
+        if(requestCode == 2){
+            String imgPath = data.getStringExtra("imagePath");
+            File file = new File(imgPath);
+            Uri imgUri = Uri.fromFile(file);
+            String[] fileString = new File(file.getParent()).list();
+            nextImage = Arrays.binarySearch(fileString,file.getName());
+            image.setImageURI(imgUri);
         }
+    }
+
+    public void photoClick(View v) {
+        dispatchTakePictureIntent();
     }
 
     @Override
     public void onClick(View v) {
+    }
 
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            }catch (IOException ex){
+                Log.d("photoFile", "Error creating image file");
+            }
+            if (photoFile != null){
+                Uri photoURI = FileProvider.getUriForFile(this,"com.example.android.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp;
+        File storageDir = new File(path);
+        //File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File photoImage = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        photoPath = photoImage.getAbsolutePath();
+        return photoImage;
     }
 }
